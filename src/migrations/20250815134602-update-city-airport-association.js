b982ded73ec75d3e40aeb949501b2c64f8a9cbd4
Hemanth <hem123.gk@gmail.com>
2025-08-15 21:27:18 +0530

    added airport associations with city

---
+++ src/migrations/20250815134602-update-city-airport-association.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint("Airports", {
+      fields: ["city_id"],
+      type: "foreign key",
+      name: "city_id_fk_constraint",
+      references: {
+        table: "Cities",
+        field: "id",
+      },
+      onDelete: "cascade",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Cities");
+  },
+};
---
+++ src/seeders/20250815153346-add-demo-cities.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Cities", [
+      {
+        name: "Bangalore",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Renigunta",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Cities", {});
+  },
+};
